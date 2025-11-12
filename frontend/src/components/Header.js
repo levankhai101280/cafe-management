a1d1c31c27f6960e5ec1ee4ef675fd153428191e
van khai <khai101280@donga.edu.vn>
2025-11-12 08:55:55 +0700

    hhhss

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -32,7 +32,7 @@ function Header({ auth }) {
         {/* Menu chính */}
         <div className="nav-links">
           <Link to="/" className="nav-link">
-            Trang Chủsdfgfdsfds
+            Trang Chủ
           </Link>
           <Link to="/products" className="nav-link">
             Sản Phẩm
